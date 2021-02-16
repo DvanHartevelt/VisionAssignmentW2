e83c0512918d4073d076a9f62a9c991406ceeef1
DvanHartevelt <416036@student.fontys.nl>
2021-02-16 21:12:48 +0100

    updated kernelSweeper, added 3 new filters.

--- localOperator/kernelSweeper.py
+++ localOperator/kernelSweeper.py
@@ -15,10 +15,11 @@ class oddkernel:
     This class contains a kernel of any odd size.
 
     Arguments:
-     self.xDim:      - Required: number of elements in the x direction
-     self.yDim:      - Required: number of elements in the y direction
-     self.valueMat:  - Required: ValueMatrix of size xDim by yDim
-     self.name:      - Optional: name of the kernel
+     self.xDim:         - Required: number of elements in the x direction
+     self.yDim:         - Required: number of elements in the y direction
+     self.valueMat:     - Required: ValueMatrix of size xDim by yDim
+     self.name:         - Optional: name of the kernel
+     self.applyAbsolute - Optional: When applied, should the filtervalue be absolute
 
     internally used variables
      self.xMiddle:      middle coordinate for x direction
@@ -30,11 +31,12 @@ class oddkernel:
 
     The method sweep() to sweep this kernel over a picture
     """
-    def __init__(self, size, valueMat, name='unnamed'):
+    def __init__(self, size, valueMat, name='unnamed', defaultNormalize=True):
         self.xDim = size[0]
         self.yDim = size[1]
         self.valueMat = valueMat
         self.name = name
+        self.defaultNormalize = defaultNormalize
 
         self.xMiddle = int((self.xDim - 1) / 2)
         self.yMiddle = int((self.yDim - 1) / 2)
@@ -44,7 +46,7 @@ class oddkernel:
         """
         Retrieves value of kernal at position i and j , with respect to the CENTER of the kernel
         :param i: horizontal integer position with respect to the center
-        :param y: vertical integer position with respect to the center
+        :param j: vertical integer position with respect to the center
         :return: value of matrix
         """
         # exception testing
@@ -70,11 +72,12 @@ class oddkernel:
 
         pass
 
-    def sweep(self, img):
+    def convolute(self, img):
         """
-        Sweeps a kernel over an image
+        Convolutes a kernel over an image
 
-        :param img: (gray) image matrix
+        :param img: REQUIRED - (gray) image matrix
+        :param img: OPTIONAL - should the matrix be normalized
         :return: sweeped image matrix
         """
 
@@ -95,20 +98,29 @@ class oddkernel:
                 # pixels multiplied by the kernels relative value.
                 newValue = 0
 
-                for j in range(-1 * self.yMiddle, self.yMiddle):
-                    for i in range(-1 * self.xMiddle, self.xMiddle):
-                        newValue += img[y + j, x + i] * self.getValue(i, j)
+                for j in range(-1 * self.yMiddle, self.yMiddle + 1):
+                    for i in range(-1 * self.xMiddle, self.xMiddle + 1):
+                        newValue += int(img[y + j, x + i]) * int(self.getValue(i, j))
+                        #print(f"Value at pos {[x, y]}, subpix {[i, j]} is {newValue}")
 
-                newValue = np.interp(newValue, [self.lowFactor*255, self.highFactor*255], [0, 255])
+                if self.defaultNormalize:
+                    newValue = np.interp(newValue, [self.lowFactor*255, self.highFactor*255], [0, 255])
+
+                if newValue < 0:
+                    newValue = 0
+                if newValue > 255:
+                    newValue = 255
 
                 imgNew[y,x] = int(newValue)
 
-            pb.printProgressBar(y + self.yMiddle, height - self.yMiddle, prefix=f'Sweeping {self.name} kernel, size {[self.xDim, self.yDim]}:', length=50)
+            pb.printProgressBar(y + self.yMiddle, height - self.yMiddle,
+                                prefix=f'Convoluting {self.name} kernel, size {[self.xDim, self.yDim]}:', length=50)
+
 
         return imgNew
 
 """
-Now to declare some commonly used kernels.
+Now to declare some commonly used kernels and filters.
 """
 kernelBlur3 = oddkernel([3, 3], [[1, 1, 1], [1, 1, 1], [1, 1, 1]], name='blurring')
 kernelBlur5 = oddkernel([5, 5], [[1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1]], name='blurring')
@@ -116,8 +128,56 @@ kernelBlur5 = oddkernel([5, 5], [[1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [1, 1, 1, 1, 
 kernelGausianLowPass3 = oddkernel([3, 3], [[1, 2, 1], [2, 4, 2], [1, 2, 1]], name='Gausian low-pass')
 kernelGausianLowPass5 = oddkernel([5, 5], [[2, 7, 12, 7, 2], [7, 31, 52, 31, 7], [12, 52, 127, 52, 12], [7, 31, 52, 31, 7], [2, 7, 12, 7, 2]], name="Gausian low-pass")
 
-kernelSquareLaplacianHighPass = oddkernel([3, 3], [[0, -1, 0], [-1, 4, -1], [0, -1, 0]], name='square Laplacian high-pass')
-kernelFullLaplacianHighPass   = oddkernel([3, 3], [[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]], name='full Laplacian high-pass')
+kernelSquareLaplacian = oddkernel([3, 3], [[0, -1, 0], [-1, 4, -1], [0, -1, 0]], name='square Laplacian (normalized)')
+kernelSquareCompositeLaplacian = oddkernel([3, 3], [[0, -1, 0], [-1, 5, -1], [0, -1, 0]], name='square Composite Laplacian', defaultNormalize=False)
+
+kernelFullLaplacian = oddkernel([3, 3], [[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]], name='full Laplacian (normalized)')
+kernelFullCompositeLaplacian = oddkernel([3, 3], [[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]], name='full Composite Laplacian', defaultNormalize=False)
+
+def PrewittOperator(img):
+    kernelPrewittX = oddkernel([3, 3], [[1, 0, -1], [1, 0, -1], [1, 0, -1]], name='Prewitt, X direction [1/3]', defaultNormalize=False)
+    kernelPrewittY = oddkernel([3, 3], [[1, 1, 1], [0, 0, 0], [-1, -1, -1]], name='Prewitt, Y direction [2/3]', defaultNormalize=False)
+
+    GX = kernelPrewittX.convolute(img)
+    GY = kernelPrewittY.convolute(img)
+
+    width = img.shape[1]
+    height = img.shape[0]
+
+    imgNew = np.zeros((height, width), np.uint8)
+
+    # Calculating magnitude
+    for y in range(0, height):
+        for x in range(0, width):
+            imgNew[y, x] = int(np.sqrt(GX[y, x] ** 2 + GY[y, x] ** 2))
+
+        pb.printProgressBar(y + 1, height,
+                            prefix=f'Combining Prewitt filter [3/3]:', length=50)
+
+    return imgNew
+
+def SobelOperator(img):
+    kernelSobelX = oddkernel([3, 3], [[1, 0, -1], [2, 0, -2], [1, 0, -1]], name='Sobel, X direction [1/3]', defaultNormalize=False)
+    kernelSobelY = oddkernel([3, 3], [[1, 2, 1], [0, 0, 0], [-1, -2, -1]], name='Sobel, Y direction [2/3]', defaultNormalize=False)
+
+    GX = kernelSobelX.convolute(img)
+    GY = kernelSobelY.convolute(img)
+
+    width = img.shape[1]
+    height = img.shape[0]
+
+    imgNew = np.zeros((height, width), np.uint8)
+
+    # Calculating magnitude
+    for y in range(0, height):
+        for x in range(0, width):
+            imgNew[y, x] = int(np.sqrt(GX[y, x] ** 2 + GY[y, x] ** 2))
+            # wikipage says it needs to be normalized, but I dont see how.
+
+        pb.printProgressBar(y + 1, height,
+                            prefix=f'Combining Sobel filter [3/3]:', length=50)
+
+    return imgNew
 
 kernelEdgeDetectionHorizontal = oddkernel([3, 3], [[-1, -1, -1], [2, 2, 2],   [-1, -1, -1]], name='horizontal edge detecting')
 kernelEdgeDetectionPlus45     = oddkernel([3, 3], [[2, -1, -1],  [-1, 2, -1], [-1, -1, 2]] , name='+45 deg. edge detecting')
--- main.py
+++ main.py
@@ -5,10 +5,10 @@ Demonstrating Local operations
 import cv2
 from localOperator import kernelSweeper as ks
 
-def query_resize(imgOrig, resizeNr):
-    smallerPic = input("Would you like to resize the picture to be smaller? [y/n]")
+def query_resize(imgOrig, resizeNr, name='unnamed'):
+    smallerPic = input(f"Would you like to resize the {name} image to be one {resizeNr ** 2}th the size? [y/n]")
     if (smallerPic.lower() == 'y'):
-        print(f"Image resized to one {resizeNr ** 2}th the size.")
+        print(f"{name.capitalize()} image resized to one {resizeNr ** 2}th the size.")
         img = cv2.resize(imgOrig, (int(imgOrig.shape[1]/resizeNr), int(imgOrig.shape[0]/resizeNr)))
         return img
     elif (smallerPic.lower() == 'n'):
@@ -19,37 +19,77 @@ def query_resize(imgOrig, resizeNr):
         return query_resize(imgOrig, resizeNr)
 
 def main():
-    print("Hello World!")
+    print("Importing pictures...")
     imgOrig = cv2.imread("Resources/smollCat.jpeg")
+    imgMoonO = cv2.imread("Resources/Moon.jpeg")
 
-    img = query_resize(imgOrig, 3)
+    imgCat = query_resize(imgOrig, 3, name='cat')
+    imgGrayCat = cv2.cvtColor(imgCat, cv2.COLOR_RGB2GRAY)
 
-    imgGray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    imgMoon = query_resize(imgMoonO, 2, name='moon')
+    imgGrayMoon = cv2.cvtColor(imgMoon, cv2.COLOR_RGB2GRAY)
 
-    if (input("Skip blurring? [y/n]").lower() != 'y'):
-        imgBlurred   = ks.kernelBlur3.sweep(imgGray)
-        imgBigBlur   = ks.kernelBlur5.sweep(imgGray)
-        imgSQLapHP   = ks.kernelSquareLaplacianHighPass.sweep(imgGray)
-        imgFullLapHP = ks.kernelFullLaplacianHighPass.sweep(imgGray)
+    if input("Skip blurring? [y/n]").lower() != 'y':
+        """
+        Demonstrating Blurring
+        """
+        imgBlurred   = ks.kernelBlur3.convolute(imgGrayCat)
+        imgBigBlur   = ks.kernelBlur5.convolute(imgGray)
 
-        cv2.imshow("Original", imgGray)
+        cv2.imshow("Original", imgGrayCat)
         cv2.imshow("Blurred 3", imgBlurred)
         cv2.imshow("Blurred 5", imgBigBlur)
-        cv2.imshow("Square Laplacian High-Pass filter", imgSQLapHP)
-        cv2.imshow("Full Laplacian High-Pass filter", imgFullLapHP)
-
-
 
         cv2.waitKey(0)
 
+    if input("Skip Laplacian? [y/n]").lower() != 'y':
+        """
+        Demonstrating Laplacian, and composite Laplacian counterparts
+        """
+        imgSquareLap   = ks.kernelSquareLaplacian.convolute(imgGrayMoon)
+        imgSquareCLap  = ks.kernelSquareCompositeLaplacian.convolute(imgGrayMoon)
+        imgFullLap     = ks.kernelFullLaplacian.convolute(imgGrayMoon)
+        imgFullCLap    = ks.kernelFullCompositeLaplacian.convolute(imgGrayMoon)
 
-    imgNorth = ks.kernelEdgeDetectionHorizontal.sweep(imgGray)
-    imgSouth = imgNorth
-    imgEast  = ks.kernelEdgeDetectionVertical.sweep(imgGray)
+        cv2.imshow("Original Moon", imgGrayMoon)
+        cv2.imshow("Square Lap. Filter", imgSquareLap)
+        cv2.imshow("Square Lap. sharpened", imgSquareCLap)
+        cv2.imshow("Full Lap. Filter", imgFullLap)
+        cv2.imshow("Full Lap. sharpened", imgFullCLap)
 
-    cv2.imshow("Original", imgGray)
-    cv2.imshow("North and South edge detection", imgNorth)
-    cv2.imshow("East edge detection", imgNorth)
+    if input("Skip Prewitt? [y,n]").lower() != 'y':
+        """
+        Demonstrating Prewitt filter
+        """
+        imgPrewitt = ks.PrewittFilter(imgGrayMoon)
+
+        cv2.imshow("Original Moon", imgGrayMoon)
+        cv2.imshow("Prewitt Moon", imgPrewitt)
+
+
+
+    # if (input("Skip blurring? [y/n]").lower() != 'y'):
+    #     imgBlurred   = ks.kernelBlur3.sweep(imgGrayCat)
+    #     #imgBigBlur   = ks.kernelBlur5.sweep(imgGray)
+    #     imgSQLapHP   = ks.kernelSquareLaplacianHighPass.sweep(imgGrayCat)
+    #     imgFullLapHP = ks.kernelFullLaplacianHighPass.sweep(imgGrayCat)
+    #
+    #     cv2.imshow("Original", imgGrayCat)
+    #     cv2.imshow("Blurred 3", imgBlurred)
+    #     #cv2.imshow("Blurred 5", imgBigBlur)
+    #
+    #
+    #
+    #     #cv2.waitKey(0)
+    #
+    #
+    # imgNorth = ks.kernelEdgeDetectionHorizontal.sweep(imgGrayCat)
+    # imgSouth = imgNorth
+    # imgEast  = ks.kernelEdgeDetectionVertical.sweep(imgGrayCat)
+    #
+    # cv2.imshow("Original", imgGrayCat)
+    # cv2.imshow("North and South edge detection", imgNorth)
+    # cv2.imshow("East edge detection", imgNorth)
 
 
     cv2.waitKey(0)
